25b6a82b521811cbf010ff8266901713e32ae069
Rogelio Mejia <Roger.Alonso7412@gmail.com>
2022-06-05 22:42:41 -0600

    Arreglado el filtrado de datos

--- application/models/Productos_model.php
+++ application/models/Productos_model.php
@@ -5,6 +5,11 @@ class Productos_model extends CI_Model{
 		$this->load->database();
 	}
   
+
+      var $table = "productos";  
+      var $select_column = array("idProducto", "producto", "idCategoria", "descripcionProducto", "precio", "existencias");  
+      var $order_column = array("idProducto", "producto", "idCategoria", "descripcionProducto", "precio", "existencias");
+
 	public function getProducto($id){
 		$this->db->where('idProducto', $id);
 		$query=$this->db->get('productos');
@@ -66,6 +71,30 @@ class Productos_model extends CI_Model{
            $this->db->select("*");  
            $this->db->from($this->table);  
            return $this->db->count_all_results();  
+      }
+
+
+	function make_query()  
+      {  
+           $this->db->select($this->select_column);  
+           $this->db->from($this->table);  
+           if(isset($_POST["search"]["value"]))  
+           {  
+                $this->db->like("idProducto", $_POST["search"]["value"]);
+                $this->db->or_like("producto", $_POST["search"]["value"]);
+                $this->db->or_like("idCategoria", $_POST["search"]["value"]); 
+                $this->db->or_like("descripcionProducto", $_POST["search"]["value"]); 
+                $this->db->or_like("precio", $_POST["search"]["value"]); 
+                $this->db->or_like("existencias", $_POST["search"]["value"]);
+           }  
+           if(isset($_POST["order"]))  
+           {  
+                $this->db->order_by($this->order_column[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);  
+           }  
+           else  
+           {  
+                $this->db->order_by('idProducto', 'ASC');  
+           }
       } 
 
 }
